107f56ced200337c72477e6f9a4f82f87f364a00
Levi <lw03232@gmail.com>
2021-02-24 20:29:53 -0600

    Custom button, hooked up GM, fixed BG

--- Unity_Project_3_Game/Assets/Scripts/GameManager.cs
+++ Unity_Project_3_Game/Assets/Scripts/GameManager.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
+
+    public GameObject startButton;
+    public GameObject backgroundImage;
+
+    public GameObject canvas;
+    public GameObject events;
+
+    public TextMeshProUGUI menuText;
+    public TextMeshProUGUI popcornText;
+
+    private int popcornCount = -1;
+    private int targetPopcorn = -2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuText.text = "AstrocorN";
+        popcornText.text = "";
     }
 
     // Update is called once per frame
