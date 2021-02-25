c3fd2c82ebf3f331ae5656a04aa6e9fcc143b67a
Levi <lw03232@gmail.com>
2021-02-24 20:43:29 -0600

    Added scene transition

--- Unity_Project_3_Game/Assets/Scripts/GameManager.cs
+++ Unity_Project_3_Game/Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,5 +32,58 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(events);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartButton()
+    {
+        startButton.SetActive(false);
+        menuText.text = "";
+        StartCoroutine(LoadYourAsyncScene("Main_Scene"));
+        popcornCount = 0;
+        targetPopcorn = 5;
+        popcornText.text = "Popcorn: " + popcornCount + "/" + targetPopcorn;
+
+    }
+
+    IEnumerator ColorLerp(Color endvalue, float duration)
+    {
+        float time = 0;
+        Image sprite = backgroundImage.GetComponent<Image>();
+        Color startValue = sprite.color;
+
+        while (time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endvalue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = endvalue;
+    }
+
+    IEnumerator LoadYourAsyncScene(string scene)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ColorLerp(new Color(0, 0, 0, 0), 2));
     }
 }
